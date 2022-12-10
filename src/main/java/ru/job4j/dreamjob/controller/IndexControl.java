11d2feab40df6f355fe916b281da413b76801bfc
mihanchik94 <mihanchik94@gmail.com>
2022-12-10 15:29:27 +0300

    2. Основы HTML [#504839]

--- src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
@@ -9,4 +9,29 @@ public class IndexControl {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/example1")
+    public String example1() {
+        return "example1";
+    }
+
+    @GetMapping("/example2")
+    public String example2() {
+        return "example2";
+    }
+
+    @GetMapping("/example3")
+    public String example3() {
+        return "example3";
+    }
+
+    @GetMapping("/example4")
+    public String example4() {
+        return "example4";
+    }
+
+    @GetMapping("/example5")
+    public String example5() {
+        return "example5";
+    }
 }
