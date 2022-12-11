69f5984c1b6b830bcc4b716de30b61cbfe7de18e
mihanchik94 <mihanchik94@gmail.com>
2022-12-11 14:48:01 +0300

    3. Bootstrap, Table [#504840]

--- src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
@@ -9,29 +9,4 @@ public class IndexControl {
     public String index() {
         return "index";
     }
-
-    @GetMapping("/example1")
-    public String example1() {
-        return "example1";
-    }
-
-    @GetMapping("/example2")
-    public String example2() {
-        return "example2";
-    }
-
-    @GetMapping("/example3")
-    public String example3() {
-        return "example3";
-    }
-
-    @GetMapping("/example4")
-    public String example4() {
-        return "example4";
-    }
-
-    @GetMapping("/example5")
-    public String example5() {
-        return "example5";
-    }
 }
